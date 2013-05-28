621fd501b00b9f1503197b21ad4a336976ed3670
Sam Neubardt <samneubardt@gmail.com>
2013-05-28 18:48:37 -0400

    Add distutils files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = "0.1.0"
+
+setup(name="run-and-report",
+      version=version,
+      description="Run a command and report it to Riemann",
+      author="Sam Neubardt",
+      author_email="samneubardt@gmail.com",
+      url="https://github.com/samn/run-and-report",
+      scripts=['run-and-report.py']
+    )
